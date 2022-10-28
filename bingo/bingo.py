192c1f4b1923a3c84777f776b868c1afd145c602
Guido E. Rodriguez <42053774+ApophisXIV@users.noreply.github.com>
2022-10-28 11:35:55 -0300

    Cardboard module

--- bingo/bingo.py
+++ bingo/bingo.py
@@ -1,26 +1,27 @@
 from random import randint
 
-def select_rnd_index(number_list, n):
-    for i in range(n):  # N_NUMBERS
-        number_list.pop(randint(0, len(number_list)-1))
-    return number_list
+def select_no_duplicates(pool,rows,n):
+    selection = []
+    for i in range(rows):
+        row = [col.pop(randint(0, len(col)-1)) for col in pool]
+        for j in range(n//rows - 1):
+            row.pop(randint(0,len(row)-1))
+        selection.append(row)
+    return selection
 
 def get_carboard():
-    carboard = []
-    for i in range(3):
-        row = []
-        selected_index = select_rnd_index(list(range(10)), 5)
-        for col in selected_index:
-            row.append(randint(1+10*col, 9+10*col))
-        carboard.append(row)
-    return carboard
+    pool = list(list(range(1 + 11 * i, 12 + 11 * i)) for i in range(9))
+    return select_no_duplicates(pool,3,15)
 
 def get_cardboards(n):
     cardboards = []
     while len(cardboards) < n:
-        cardboad = get_carboard()
-        if cardboad not in cardboards:
-            cardboards.append(cardboad)
+        cardboard = get_carboard()
+        if cardboard not in cardboards:
+            print("CARTON")
+            print (cardboard)
+            cardboards.append(cardboard)
     return cardboards
 
-print(get_cardboards(5))
+get_cardboards(5)
+#print(get_cardboards(5))
